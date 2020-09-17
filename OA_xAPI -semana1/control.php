db561eccf7b87718dda79f7921037bdcd7a23eb1
enzorusso <enzorussor1@gmail.com>
2020-09-17 16:37:48 -0300

    inicializando repo

---
+++ OA_xAPI -semana1/control.php
@@ -0,0 +1,134 @@
+<?php
+session_start();
+$message = '';
+$error = '';
+$file = 'xAPI.json';
+$nomeObjeto = "ALGORITMOS E PROGRAMACAO I";
+
+
+$data =  date('l jS \of F Y h:i:s A');
+    
+        if(file_exists($file))
+        {
+            //Trata informação de ID e nome
+            if(!empty($_POST["nome"]) && !empty($_POST["inicio"])){
+                $verbo = "Acessou o Objeto ".$nomeObjeto;
+
+                $current_data = file_get_contents($file);
+                $array_data = json_decode($current_data, true);
+                $extra = array (
+                    'nome' => $_POST["nome"],
+                    'verbo' => $verbo,
+                    'data' => $data
+                );
+                $array_data[] = $extra;
+                $final_data = json_encode($array_data);
+                if(file_put_contents($file, $final_data))
+                {   
+                    $message = "<label class='text-success'>Sucesso!</p>";
+                }   
+            }
+
+             //Trata informação de ID e pagina
+             if(!empty($_POST["nome"]) && !empty($_POST["pagina"])){
+                $verbo = "Acessou a pagina ".$_POST["pagina"];
+
+                $current_data = file_get_contents($file);
+                $array_data = json_decode($current_data, true);
+                $extra = array (
+                    'nome' => $_POST["nome"],
+                    'verbo' => $verbo,
+                    'data' => $data
+                );
+                $array_data[] = $extra;
+                $final_data = json_encode($array_data);
+                if(file_put_contents($file, $final_data))
+                {   
+                    $message = "<label class='text-success'>Sucesso!</p>";
+                }   
+            }
+
+            //INICIO ENZO
+
+            //Trata informação de ID e play no audio
+            if(!empty($_POST["nome"]) && !empty($_POST["audio_play"])){
+                $verbo = "Deu play no audio ".$_POST["audio_play"];
+
+                $current_data = file_get_contents($file);
+                $array_data = json_decode($current_data, true);
+                $extra = array (
+                    'nome' => $_POST["nome"],
+                    'verbo' => $verbo,
+                    'data' => $data
+                );
+                $array_data[] = $extra;
+                $final_data = json_encode($array_data);
+                if(file_put_contents($file, $final_data))
+                {   
+                    $message = "<label class='text-success'>Sucesso!</p>";
+                }   
+            }
+
+            //Trata informação de ID e pause no audio
+            if(!empty($_POST["nome"]) && !empty($_POST["audio_pause"])){
+                $verbo = "Deu pause no audio ".$_POST["audio_pause"];
+
+                $current_data = file_get_contents($file);
+                $array_data = json_decode($current_data, true);
+                $extra = array (
+                    'nome' => $_POST["nome"],
+                    'verbo' => $verbo,
+                    'data' => $data
+                );
+                $array_data[] = $extra;
+                $final_data = json_encode($array_data);
+                if(file_put_contents($file, $final_data))
+                {   
+                    $message = "<label class='text-success'>Sucesso!</p>";
+                }   
+            }
+
+            //Trata informação de ID e play no video
+            if(!empty($_POST["nome"]) && !empty($_POST["video_play"])){
+                $verbo = "Deu play no video ".$_POST["video_play"];
+
+                $current_data = file_get_contents($file);
+                $array_data = json_decode($current_data, true);
+                $extra = array (
+                    'nome' => $_POST["nome"],
+                    'verbo' => $verbo,
+                    'data' => $data
+                );
+                $array_data[] = $extra;
+                $final_data = json_encode($array_data);
+                if(file_put_contents($file, $final_data))
+                {   
+                    $message = "<label class='text-success'>Sucesso!</p>";
+                }   
+            }
+
+            //Trata informação de ID e pause no video
+            if(!empty($_POST["nome"]) && !empty($_POST["video_pause"])){
+                $verbo = "Deu pause no audio ".$_POST["video_pause"];
+
+                $current_data = file_get_contents($file);
+                $array_data = json_decode($current_data, true);
+                $extra = array (
+                    'nome' => $_POST["nome"],
+                    'verbo' => $verbo,
+                    'data' => $data
+                );
+                $array_data[] = $extra;
+                $final_data = json_encode($array_data);
+                if(file_put_contents($file, $final_data))
+                {   
+                    $message = "<label class='text-success'>Sucesso!</p>";
+                }   
+            }
+            //FIM ENZO
+        } else {
+            echo "Arquivo JSON não existe!";
+        }
+
+
+?>
